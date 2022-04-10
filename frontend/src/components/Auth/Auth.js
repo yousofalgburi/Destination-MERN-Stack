4936d319e2a9dc509e6fb1e1b92f1ba845dfc1ab
Yousof Algburi <75765571+yousofalgburi@users.noreply.github.com>
2022-04-10 00:18:43 -0400

    create grid for Auth page

--- frontend/src/components/Auth/Auth.js
+++ frontend/src/components/Auth/Auth.js
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { useHistory } from 'react-router-dom'
 import { signin, signup } from '../../actions/auth'
-import { FormControl, FormLabel, FormErrorMessage, FormHelperText, Text, Input, Button, Container, VStack } from '@chakra-ui/react'
+import { FormControl, FormLabel, FormErrorMessage, FormHelperText, Text, Input, Button, Container, VStack, Heading, SimpleGrid, GridItem } from '@chakra-ui/react'
 
 const Auth = () => {
     const [form, setForm] = useState({ firstName: '', lastName: '', email: '', password: '', confirmPassword: '' })
@@ -32,35 +32,50 @@ const Auth = () => {
     return (
         <Container p={0}>
             <FormControl onSubmit={handleSubmit} isInvalid={isError} py={20}>
-                <VStack w="full" h="full" p={10} spacing={5} bg="gray.50">
-                    <Text>{ isSignup ? 'Sign in' : 'Sign up' }</Text>
-                    <div>
+                <VStack w="full" h="full" p={10} spacing={10} bg="gray.50" alignItems="flex-start">
+                    <Heading>{ isSignup ? 'Sign in' : 'Sign up' }</Heading>
+
+                    <SimpleGrid columns={2} columnGap={3} rowGap={6} w="full">
                         { isSignup && (
                             <>
-                                <div>
+                                <GridItem colSpan={1}>
                                     <FormLabel htmlFor='firstname'>First Name</FormLabel>
-                                    <Input id='firstname' placeholder='first name' type='text' name='firstName' onChange={handleChange} required/>  
-                                </div>
-
-                                <div>
+                                    <Input id='firstname' placeholder='John' type='text' name='firstName' onChange={handleChange} value={form.firstName} required/> 
+                                </GridItem>
+                                
+                                <GridItem colSpan={1}>
                                     <FormLabel htmlFor='lastname'>Last Name</FormLabel>
-                                    <Input id='lastname' placeholder='last name' type='text' name='lastName' onChange={handleChange} required/>
-                                </div>
+                                    <Input id='lastname' placeholder='Williams' type='text' name='lastName' onChange={handleChange} value={form.lastName} required/>
+                                </GridItem>
                             </>
                         )}
-                        <div>
-                            <label htmlFor='email'>Email</label>
-                            <Input id='email' placeholder='email' type='email' name='email' onChange={handleChange} required/>
-                        </div>
 
-                        <div>
-                            <label htmlFor='password'>Password</label>
-                            <Input id='password' placeholder='password' type='password' name='password' onChange={handleChange} required/>
-                        </div>
-                    </div>
+                        <GridItem colSpan={2}>
+                            <FormLabel htmlFor='email'>Email</FormLabel>
+                            <Input id='email' placeholder='Example@gmail.com' type='email' name='email' onChange={handleChange} value={form.email} required/>
+                        </GridItem>
+                        
+                        <GridItem colSpan={isSignup ? 1 : 2}>
+                            <FormLabel htmlFor='password'>Password</FormLabel>
+                            <Input id='password' placeholder='Password' type='password' name='password' onChange={handleChange} value={form.password} required/>
+                        </GridItem>
 
-                    <Button type='submit'>{isSignup ? 'Signup' : 'Signin'}</Button>
-                    <Button onClick={switchMode}>{ isSignup ? 'Sign In Instead' : 'Signup Instead'}</Button>
+                        {isSignup &&
+                            <GridItem colSpan={1}>
+                                <FormLabel htmlFor='confirmPassword'>Confirm Password</FormLabel>
+                                <Input id='confirmPassword' placeholder='Password Again' type='password' name='confirmPassword' onChange={handleChange} value={form.confirmPassword} required/>
+                            </GridItem>
+                         }
+
+                        <GridItem colSpan={2}>
+                            <Button w="full" size="lg" type='submit'>{isSignup ? 'Signup' : 'Signin'}</Button>
+                        </GridItem>
+
+                        <GridItem colSpan={2}>
+                            <Button w="full" size="lg" onClick={switchMode}>{ isSignup ? 'Sign In Instead' : 'Signup Instead'}</Button>
+                        </GridItem>
+
+                    </SimpleGrid>
                 </VStack>
             </FormControl>
         </Container>
